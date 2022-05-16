5117ff81307b3c2da953d6aac18e8d612ebec7fb
Ninhache <mr.carambar27@gmail.com>
2022-05-16 07:58:45 +0000

    Update submodules

--- src/main.js
+++ src/main.js
@@ -1,26 +1,28 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const port = 3000;
+const port = 80;
 
 // list of routers
 const jSeriesRouter = require('./routes/JustSeries');
 const portfolioRouter = require('./routes/Portfolio');
 const resumeRouter = require('./routes/Resume');
+const mazeRouter = require('./routes/MazeThings');
 
 
 app.use(express.static(__dirname + '/public'));
 
 app.get(/^\/$/, (req, res) => {
-    res.redirect(301, '/portfolio');
+    res.redirect(301, '/Portfolio');
 });
 
 app.use('/Portfolio', portfolioRouter);
 app.use('/JustSeries', jSeriesRouter);
 app.use('/Resume', resumeRouter);
+app.use('/MazeThings', mazeRouter);
 
 app.all('*', (req, res) => {
-    res.redirect(301, '/portfolio');
+    res.redirect(301, '/Portfolio');
 }
 );
 
